f2ab34346716a313c847b77e4139ee0c2bd1dca7
mohsine-msd <129232615+mohsine-msd@users.noreply.github.com>
2025-11-19 13:15:11 +0100

    Implement loading state with image preloading
    Added loading state and image preloading logic to improve user experience.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,50 @@
-import { useState } from 'react'
-import Nav from './comp/Nav'
-import Hero from './comp/hero'
+import { useState, useEffect } from 'react'
+import Hero from './comp/Hero'
 import Program from './comp/Program'
 import About from './comp/About'
 import Testimonials from './comp/Testimonials'
 import Contact from './comp/Contact'
+import LoadingPage from './comp/Loader'
+import studentsImg from './assets/free-time-students-bachelor-s-campus-life-rhythm-five-friendly-students-are-walking.jpg';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const imagesToPreload = [studentsImg];
+
+    let loadedImages = 0;
+    const totalImages = imagesToPreload.length;
+
+    imagesToPreload.forEach((src) => {
+      const img = new Image();
+      img.src = src;
+      img.onload = () => {
+        loadedImages++;
+        if (loadedImages === totalImages) {
+          setTimeout(() => setLoading(false), 1000);
+        }
+      };
+      img.onerror = () => {
+        loadedImages++;
+        if (loadedImages === totalImages) {
+          setTimeout(() => setLoading(false), 1000);
+        }
+      };
+    });
+  }, []);
 
   return (
     <>
-          
-
-     <Hero />
-     <Program />
-     <About />
-     <Testimonials />
-     <Contact />
-     
+      {loading ? <LoadingPage /> : (
+        <>
+          <Hero />
+          <Program />
+          <About />
+          <Testimonials />
+          <Contact />
+        </>
+      )}
     </>
   )
 }
